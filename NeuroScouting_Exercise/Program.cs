014f856a19f6babf8d626dd158f65532cb7c1c05
Nishit Savla <Nishit Savla>
2016-10-23 10:18:41 -0400

    Main Class Declared

--- NeuroScouting_Exercise/Program.cs
+++ NeuroScouting_Exercise/Program.cs
@@ -10,6 +10,20 @@ namespace NeuroScouting_Exercise
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the Height");
+            string tempString = Console.ReadLine();
+            int height = Int32.Parse(tempString);
+            Console.WriteLine("Height =" + height);
+            BSTNode root = null;
+            //Some function which will be later declared.
+            root = MakeTree(root, 4);
+            System.Console.ReadKey();
+        }
+
+        static BSTNode MakeTree(BSTNode root, int level)
+        {
+            // Empty for now.
+            return root;
         }
     }
 }
